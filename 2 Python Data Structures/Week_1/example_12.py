4c64719420bdee5edb00b16c054e21bfdaab814a
Lincoln Johnny <lincolnjohnny@gmail.com>
2020-10-04 15:23:49 -0300

    Create example_12.py

---
+++ 2 Python Data Structures/Week_1/example_12.py
@@ -0,0 +1,13 @@
+# String comparison
+
+word = 'abnana'
+
+if word == 'banana' :
+    print('All right, bananas.')
+
+if word < 'banana' :
+    print('Your word, ' + word + ', comes before banana.')
+elif word > 'banana' :
+    print('Your word, ' + word + ', comes after banana.')
+else:
+    print('All right, bananas.')
