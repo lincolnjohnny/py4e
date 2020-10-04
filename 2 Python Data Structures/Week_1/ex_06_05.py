ae8acc6146a8d798a6c9790eb835e8c760f9f72c
Lincoln Johnny <lincolnjohnny@gmail.com>
2020-10-04 19:34:42 -0300

    Create ex_06_05.py

---
+++ 2 Python Data Structures/Week_1/ex_06_05.py
@@ -0,0 +1,11 @@
+# Write code using find() and string slicing (see section 6.10) to extract the number at the 
+# end of the line below. Convert the extracted value to a floating point number and print it out.
+# Desired Output:
+# 0.8475
+
+text = "X-DSPAM-Confidence:    0.8475"
+position = text.find('0.8475')
+float(position)
+print(text[position:position+6])
+
+
