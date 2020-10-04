d327c41285e4f2ef50800b6dabe01c84137da6b9
Lincoln Johnny <lincolnjohnny@gmail.com>
2020-10-04 17:49:11 -0300

    Create example_17.py

---
+++ 2 Python Data Structures/Week_1/example_17.py
@@ -0,0 +1,12 @@
+# Stripping Whitespace
+
+greet = '          Hello Bob     '
+
+# strip on left
+print(greet.lstrip())
+
+# strip on right
+print(greet.rstrip())
+
+#strip on left and right
+print(greet.strip())
