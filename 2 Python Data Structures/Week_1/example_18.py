17523855a8a47dbe6e808ae49922037b26b1d2eb
Lincoln Johnny <lincolnjohnny@gmail.com>
2020-10-04 18:06:15 -0300

    Create example_18.py

---
+++ 2 Python Data Structures/Week_1/example_18.py
@@ -0,0 +1,9 @@
+# Prefixes
+
+line = 'Please have a nice day.'
+
+# verify if the line starts with the given parameter ('Please')
+print(line.startswith('Please'))
+
+# verify if the line starts with the given parameter ('p')
+print(line.startswith('p'))
