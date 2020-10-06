d15acf11585f6cc75623956c386bde9cd4772700
Lincoln Johnny <lincolnjohnny@gmail.com>
2020-10-06 19:55:35 -0300

    Create example_01.py

---
+++ 2_Python_Data_Structures/Week_4/example_01.py
@@ -0,0 +1,13 @@
+# Using the Range function
+
+# I get a list of 4 numbers from 0 to 3
+print(range(4))
+
+# there are 3 elements in the list
+friends = ['Joseph', 'Gleen', 'Sally']
+
+# I get the length "3" of the variable "friends"
+print(len(friends))
+
+# I get a list of 3 numbers from 0 to 2
+print(range(len(friends)))
