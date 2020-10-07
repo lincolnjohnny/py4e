6b42e3b91055010ad7a5b5d1a453311a0258273c
Lincoln Johnny <lincolnjohnny@gmail.com>
2020-10-07 09:48:52 -0300

    Create example_10.py

---
+++ 2_Python_Data_Structures/Week_4/example_10.py
@@ -0,0 +1,13 @@
+# Best Friends: Strings and Lists
+
+abc = 'With three words'
+
+# split the string abc into a list
+stuff = abc.split()
+
+print(stuff)
+print(len(stuff))
+print(stuff[0])
+
+for word in stuff :
+    print(word)
