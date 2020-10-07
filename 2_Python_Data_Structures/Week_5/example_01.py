84640bbca8e54bbdfd389a8205d1b0cc2735f2a6
Lincoln Johnny <lincolnjohnny@gmail.com>
2020-10-07 14:24:27 -0300

    Create example_01.py

---
+++ 2_Python_Data_Structures/Week_5/example_01.py
@@ -0,0 +1,10 @@
+# Dictionaries
+
+purse = dict()
+purse['money'] = 12
+purse['candy'] = 3
+purse['tissues'] = 75
+print('What do I have in my purse?:', purse)
+print('How many candies I have?', purse['candy'])
+purse['candy'] = purse['candy'] + 2
+print('What do I have in my purse now?:', purse)
