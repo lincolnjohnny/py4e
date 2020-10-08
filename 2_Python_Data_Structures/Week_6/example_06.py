7efa9be9d8c5ed6936814635c78bd9b7d7dad1c7
Lincoln Johnny <lincolnjohnny@gmail.com>
2020-10-08 12:37:02 -0300

    Create example_06.py

---
+++ 2_Python_Data_Structures/Week_6/example_06.py
@@ -0,0 +1,13 @@
+# Tuples and dictionaries
+
+d = dict()
+d['csev'] = 2
+d['cwen'] = 4
+for k,v in d.items() :
+    print(k, v)
+# csev 2
+# cwen 4
+
+tups = d.items()
+print(tups)
+# dict_items([('csev', 2), ('cwen', 4)]) this is a list of tuples
