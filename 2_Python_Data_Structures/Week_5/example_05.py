e7bce4db95540076bf6b57e78fab952fe650554a
Lincoln Johnny <lincolnjohnny@gmail.com>
2020-10-07 22:20:17 -0300

    Create example_05.py

---
+++ 2_Python_Data_Structures/Week_5/example_05.py
@@ -0,0 +1,10 @@
+# When we see a new name
+
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names :
+    if name not in counts:
+        counts[name] = 1
+    else:
+        counts[name] = counts[name] + 1
+print(counts)
