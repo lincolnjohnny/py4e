453440b4d60417ee2990d8f8bce1172380eecacb
Lincoln Johnny <lincolnjohnny@gmail.com>
2020-10-07 22:20:13 -0300

    Create example_04.py

---
+++ 2_Python_Data_Structures/Week_5/example_04.py
@@ -0,0 +1,12 @@
+# Dictionaries tracebacks
+
+ccc = dict()
+
+# empty dictionary
+print(ccc)
+
+# doesn't exist yet
+print(ccc['csev'])
+
+# FALSE because this really doesn't exist :-)
+print('csev' in ccc)
