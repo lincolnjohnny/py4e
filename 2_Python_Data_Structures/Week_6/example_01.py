b976f07cd1dc7002285f361e36e6eb3e2a822dfb
Lincoln Johnny <lincolnjohnny@gmail.com>
2020-10-08 12:05:27 -0300

    Create example_01.py

---
+++ 2_Python_Data_Structures/Week_6/example_01.py
@@ -0,0 +1,18 @@
+# Tuples are like Lists
+
+x = ('Glenn', 'Sally', 'Joseph')
+print(x[2])
+# Joseph
+
+y = (1, 9, 2)
+print(y)
+# (1, 9, 2)
+
+print(max(y))
+# 9
+
+for iter in y :
+    print(iter)
+# 1
+# 9
+# 2
