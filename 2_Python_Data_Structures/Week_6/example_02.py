57ce4dcb9a45bb8806a239c87ad624a09d9539eb
Lincoln Johnny <lincolnjohnny@gmail.com>
2020-10-08 12:05:33 -0300

    Create example_02.py

---
+++ 2_Python_Data_Structures/Week_6/example_02.py
@@ -0,0 +1,14 @@
+# but... Tuples are "immutable"
+
+x = [9, 8, 7]
+x[2] = 6
+print(x)
+# [9, 8, 6]
+
+y = 'ABC'
+y[2] = 'D'
+# # TypeError: 'str' object does not support item assignment
+
+z = (5, 4, 3)
+z[2] = 0
+# TypeError: 'tuple' object does not support item assignment
