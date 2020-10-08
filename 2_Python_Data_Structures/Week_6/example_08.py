3c9c5c4708cd7aba46d857c33dd26248ee816140
Lincoln Johnny <lincolnjohnny@gmail.com>
2020-10-08 12:37:09 -0300

    Create example_08.py

---
+++ 2_Python_Data_Structures/Week_6/example_08.py
@@ -0,0 +1,8 @@
+# Sorting Lists of Tuples
+
+d = {'a': 10, 'b': 1, 'c': 22}
+print(d.items())
+# dict_items([('a', 10), ('b', 1), ('c', 22)])
+
+print(sorted(d.items()))
+# [('a', 10), ('b', 1), ('c', 22)]
